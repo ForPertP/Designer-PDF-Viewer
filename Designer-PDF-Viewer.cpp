f1842df6790e88d64081816a9dfdb5812a0643fb
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-12-13 02:12:54 +0900

    Update Designer-PDF-Viewer.cpp

--- Designer-PDF-Viewer.cpp
+++ Designer-PDF-Viewer.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'designerPdfViewer' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h
+ *  2. STRING word
+ */
+
 int designerPdfViewer(vector<int> h, string word)
 {
     int n = word.length();
